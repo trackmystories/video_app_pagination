0db4f50c609c96f756f524165b75dc8a58a0578a
trackmystories <ashirazee@gmail.com>
2021-04-13 23:47:39 +0500

    Update RNVideo

--- src/components/RNVideo.js
+++ src/components/RNVideo.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import * as React from 'react';
 import {StyleSheet, View, Image} from 'react-native';
 import {WebView} from 'react-native-webview';
 import {RNVideoProps} from '../types/types';
